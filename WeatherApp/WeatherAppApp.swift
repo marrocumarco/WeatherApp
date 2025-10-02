f54c3436bc631caf0cdb5413fa5fdedf45c6a715
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-10-02 14:09:44 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by maomar on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
