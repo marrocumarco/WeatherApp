f9ee92b8ac098d7f29b8d8d9c27a61cdd186644a
Marco Marrocu <marrocumarcozaggi@gmail.com>
2026-01-21 18:51:22 +0100

    created dependency injection container

---
+++ WeatherApp/Utilities/DependencyInjectionContainer.swift
@@ -0,0 +1,12 @@
+//
+//  DependencyInjectionContainer.swift
+//  WeatherApp
+//
+//  Created by marrocumarco on 21/01/2026.
+//
+
+import Foundation
+
+struct DependencyInjectionContainer {
+    
+}
---
+++ WeatherAppTests/DependencyInjectionContainerTests.swift
@@ -0,0 +1,22 @@
+//
+//  DependencyInjectionContainerTests.swift
+//  WeatherAppTests
+//
+//  Created by marrocumarco on 21/01/2026.
+//
+@testable import WeatherApp
+import Testing
+
+struct DependencyInjectionContainerTests {
+
+    var sut: DependencyInjectionContainer!
+    
+    init() {
+        sut = DependencyInjectionContainer()
+    }
+
+//    @Test func <#test function name#>() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+
+}
